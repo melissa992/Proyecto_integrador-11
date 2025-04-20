1c8db753aab88f73ebd5fe9485b86fa7ed3a554d
trijuank <juan.lopez.quintana@correounivalle.edu.co>
2025-04-20 17:19:11 -0500

    logo header

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router";
 import "./Header.css";
+import logo from "/public/cm.png"; // Ajusta la ruta al ícono según tu estructura
 
 const Header = () => {
   return (
     <header>
       <nav>
+        <img src={logo} alt="CM Software" className="logo" />
         <NavLink to="/" end>
           Inicio
         </NavLink>
