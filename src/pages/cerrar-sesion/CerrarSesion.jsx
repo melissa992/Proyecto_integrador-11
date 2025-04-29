26dd9e4b8b4533f0f0a2533abbf3d93c05e11e9c
melissa992 <melyocoro@gmail.com>
2025-04-29 13:25:23 -0500

    se ajusta modulo arritmia

--- src/pages/cerrar-sesion/CerrarSesion.jsx
+++ src/pages/cerrar-sesion/CerrarSesion.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CerrarSesion.css";
 
 const CerrarSesion = () => {
   return <div>Cerrar Sesion</div>;
--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,34 +1,7 @@
 import React from "react";
-import "./Inicio.css"; // Ajusta la ruta según tu estructura
 
 const Inicio = () => {
-  return (
-    <>
-      <div className="corazon-title">Corazón</div>
-      <div className="description-title">
-        <h2>Sobre Nosotros</h2>
-        <p>
-          En CM, somos un equipo de expertos en desarrollo web desde el año
-          2017, apasionados por la tecnología y la innovación. Nos
-          especializamos en la creación de sitios web y aplicaciones web
-          modernas, optimizadas y escalables. Utilizamos tecnologías de
-          vanguardia como JavaScript, HTML, CSS, Angular, React, Node.js, Spring
-          Boot, MySQL y MongoDB, combinando buenas prácticas de desarrollo con
-          metodologías ágiles para garantizar productos de alta calidad. Nuestro
-          enfoque se centra en la experiencia del usuario (UX/UI), rendimiento y
-          seguridad, asegurando que cada proyecto cumpla con los más altos
-          estándares. Desde páginas web corporativas hasta plataformas
-          interactivas y sistemas web avanzados, en CM transformamos ideas en
-          soluciones digitales efectivas. Si buscas un equipo comprometido con
-          la excelencia en desarrollo web, estamos aquí para ayudarte.
-          ¡Construyamos juntos la web del futuro!
-        </p>
-      </div>
-      <div className="experience-title">
-        <h2>Viva una experiencia 3D del Corazón</h2>
-      </div>
-    </>
-  );
+  return <div>Inicio</div>;
 };
 
 export default Inicio;
